9a1adc689fa922835635fcaf7fd513d5967e4413
hema29 <31930095+hema29@users.noreply.github.com>
2017-09-26 13:36:23 +0530

    Add files via upload

---
+++ withoutsemicolon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Print_Without_Semicolon 
+{
+     public static void main(String[] args) 
+     {
+        int i = 0;
+        if(System.out.printf("Sanfoundary!! ") == null) {}
+        for(i = 1; i < 2; System.out.println("Hello World."))
+        {
+            i++;
+     	}
+     }
+}
